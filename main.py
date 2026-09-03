88f8975433ff1f3bef4f9ff1d786354bcf16342b
daanniill <daniil.malakhov12@gmail.com>
2026-09-03 16:15:29 -0700

    compared highest variance against ground truths, determined 90% success rate

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 from pathlib import Path
 from itertools import islice
 import numpy as np
@@ -51,10 +52,33 @@ for img_set, quadrants in dataset.items():
         print(f"Error processing {img_name} in {img_set}/{quadrant}: {e}")
 
 # print the results
+ground_truth = {}
+
+with open("ground_truth_files.csv", newline="") as f:
+  for row in csv.DictReader(f):
+    key = (row["folder"], row["quadrant"])
+    ground_truth[key] = row["ground_truth_filename"]
+
+correct = 0
+total = 0
+
+
 for img_set, quadrants in results.items():
-  print(f"Results for {img_set}:")
   for quadrant, variances in quadrants.items():
-    names = [img_name for img_name, _ in variances]
-    print(f"  Quadrant {quadrant}: files={names}")
-    for img_name, variance in variances:
-      print(f"    {img_name}: {variance}")
+    key = (img_set, quadrant)
+    focused_img = ground_truth.get(key)
+
+    pred_file, highest_var = max(variances, key=lambda item: item[1])
+
+    true_file = ground_truth[(img_set, quadrant)]
+    is_correct = pred_file == true_file
+
+    correct += int(is_correct)
+    total += 1
+
+
+success_rate = correct / total * 100
+
+print(f"\nCorrect: {correct}/{total}")
+print(f"Incorrect: {total - correct}/{total}")
+print(f"Success rate: {success_rate:.1f}%")
